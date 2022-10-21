04761b0639dda1de32a300a1bc0284ee84f49755
devilhacksyt <116340279+devilhacksyt@users.noreply.github.com>
2022-10-21 14:25:01 +0530

    Add files via upload

---
+++ keytime.sh
@@ -0,0 +1,10 @@
+read -p "Enter Key: " key
+x=$(echo $key | tail -c +3 | head -c -3 | base64 -d | jq -r '.Authorization.Expires')
+y=$(echo $x | tr "TZ" " ")
+z=$(echo $y UTC)
+val=$(date -d "$z")
+echo
+echo
+echo "Script by Telegram @mr_devil_hacks"
+echo "---------------"
+echo "Key will Expire on Time: $val"
